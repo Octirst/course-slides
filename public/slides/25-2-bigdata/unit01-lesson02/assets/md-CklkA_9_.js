b786e91e68ed0c3badec3d7027a7f1a13badd1f1
Kai <lidesery4430@gmail.com>
2026-02-16 14:24:04 +0800

    Fix slides: highlight range, remove breaks, fix diagram alignment

---
+++ public/slides/25-2-bigdata/unit01-lesson02/assets/md-CklkA_9_.js
@@ -0,0 +1,13 @@
+import{_ as r}from"./slidev/CodeBlockWrapper.vue_vue_type_script_setup_true_lang-B1mg-Nxj.js";import{o,b as c,w as l,g as s,d as u,m as d,C as n,v as m,x as f,z as e}from"./modules/vue-DjIz6fym.js";import{I as k}from"./slidev/default-DZr8mOL0.js";import{u as _,f as g}from"./slidev/context-B9uwSLGb.js";import"./modules/unplugin-icons-C2ygb8gq.js";import"./index-Bn0ipkaf.js";import"./modules/shiki-DEPxurZf.js";const I={__name:"slides.md__slidev_27",setup(x){const{$clicksContext:t,$frontmatter:i}=_();return t.setup(),(h,a)=>{const p=r;return o(),c(k,m(f(e(g)(e(i),26))),{default:l(()=>[a[1]||(a[1]=s("h1",null,"🗺️ 单元一 知识图谱",-1)),u(p,d({},{title:"",ranges:[]}),{default:l(()=>[...a[0]||(a[0]=[s("pre",{class:"shiki shiki-themes vitesse-dark vitesse-light slidev-code",style:{"--shiki-dark":"#dbd7caee","--shiki-light":"#393a34","--shiki-dark-bg":"#121212","--shiki-light-bg":"#ffffff"}},[s("code",{class:"language-text"},[s("span",{class:"line"},[s("span",null,"Unit 1: 初识大数据与环境准备")]),n(`
+`),s("span",{class:"line"},[s("span",null,"├── 第1次课：认知 + 入门")]),n(`
+`),s("span",{class:"line"},[s("span",null,"│   ├── 大数据 4V 特征")]),n(`
+`),s("span",{class:"line"},[s("span",null,"│   ├── Hadoop 生态全景")]),n(`
+`),s("span",{class:"line"},[s("span",null,"│   ├── MobaXterm 远程连接")]),n(`
+`),s("span",{class:"line"},[s("span",null,"│   └── Linux 基础命令 (ls/cd/pwd/cat/mkdir)")]),n(`
+`),s("span",{class:"line"},[s("span",null,"│")]),n(`
+`),s("span",{class:"line"},[s("span",null,"└── 第2次课：进阶 + 网络")]),n(`
+`),s("span",{class:"line"},[s("span",null,"    ├── Linux 文件系统结构")]),n(`
+`),s("span",{class:"line"},[s("span",null,"    ├── 文件操作命令 (cp/mv/rm/vi)")]),n(`
+`),s("span",{class:"line"},[s("span",null,"    ├── VMware 网络模式")]),n(`
+`),s("span",{class:"line"},[s("span",null,"    ├── 静态 IP 配置")]),n(`
+`),s("span",{class:"line"},[s("span",null,"    └── 网络配置体验实验")])])],-1)])]),_:1},16)]),_:1},16)}}};export{I as default};
