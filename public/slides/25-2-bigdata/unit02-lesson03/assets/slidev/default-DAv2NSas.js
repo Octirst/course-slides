fd49fc9330b6107edae59094f5e3d6a1a508e789
Kai <lidesery4430@gmail.com>
2026-02-22 22:09:52 +0800

    fix: slide 8 code highlight range for SSH commands

---
+++ public/slides/25-2-bigdata/unit02-lesson03/assets/slidev/default-DAv2NSas.js
@@ -0,0 +1,1 @@
+import{o,e as t,i as s}from"../modules/vue-zGeBh3yR.js";import{_ as r}from"../index-Drq5cUoo.js";const c={},a={class:"slidev-layout default"};function n(e,l){return o(),t("div",a,[s(e.$slots,"default")])}const f=r(c,[["render",n]]);export{f as I};
