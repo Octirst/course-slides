017be149e852f79b4ff24af5d370ce578b1e08a4
Kai <lidesery4430@gmail.com>
2026-03-11 22:29:35 +0800

    feat(bigdata): update lesson 01 PPT with local case and fixed assets

--- public/slides/25-2-bigdata/unit01-lesson01/assets/slidev/default-CUcAh8Bd.js
+++
@@ -1,1 +0,0 @@
-import{o,e as t,i as s}from"../modules/vue-MxEj1wwt.js";import{_ as r}from"../index-VkazmFiz.js";const c={},a={class:"slidev-layout default"};function n(e,l){return o(),t("div",a,[s(e.$slots,"default")])}const f=r(c,[["render",n]]);export{f as I};
